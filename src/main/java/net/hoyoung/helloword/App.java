5a0b0937c69ca1a16350ed9451cbd2476a82676d
HuYang <huyang09@internal.baidu.com>
2016-12-04 23:54:29 +0800

    hi

---
+++ src/main/java/net/hoyoung/helloword/App.java
@@ -0,0 +1,28 @@
+package net.hoyoung.helloword;
+
+import akka.actor.ActorSystem;
+import akka.actor.UntypedActor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        ActorSystem system = ActorSystem.create("hello-akka");
+//        system.actorOf(Props)
+    }
+    public static class Greeter extends UntypedActor{
+
+        @Override
+        public void onReceive(Object o) throws Exception {
+            if(o instanceof String){
+                System.out.println(o);
+            }
+        }
+    }
+}
